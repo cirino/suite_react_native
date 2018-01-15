906099338adb3b6bc9c987e77aba1d051e8065a0
Dagmar Cirino <dagmar.mv@gmail.com>
2018-01-15 18:03:25 +0000

    Update index.js

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -15,60 +15,11 @@ export default class ProductList extends Component {
 	}
 
 	componentWillMount() {
-		//requisção HTTP
 		axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
 			.then(response => { this.setState({ produtosItens: response.data }); })
 			.catch(() => { console.log('Erro ao recuperar os dados'); });
 	}
 
-  /*
-  state = {
-        products: [
-          {
-            id: 1,
-            image: 'https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=28010875',
-            title: 'Acne Studios',
-            description: 'Andrea nappa dusty pink',
-            price: 'R$50,00',
-          },
-          {
-            id: 2,
-            image: 'https://images.neimanmarcus.com/ca/1/product_assets/B/3/T/5/S/NMB3T5S_au.jpg',
-            title: 'Acne Studios',
-            description: 'Lain pop sky blue',
-            price: 'R$70,00',
-          },
-          {
-            id: 3,
-            image: 'https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=28010875',
-            title: 'Acne Studios',
-            description: 'Andrea nappa dusty pink',
-            price: 'R$30,00',
-          },
-          {
-            id: 4,
-            image: 'https://images.neimanmarcus.com/ca/1/product_assets/B/3/T/5/S/NMB3T5S_au.jpg',
-            title: 'Acne Studios',
-            description: 'Lain pop sky blue',
-            price: 'R$70,00',
-          },
-          {
-            id: 5,
-            image: 'https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=28010875',
-            title: 'Acne Studios',
-            description: 'Andrea nappa dusty pink',
-            price: 'R$30,00',
-          },
-          {
-            id: 6,
-            image: 'https://images.neimanmarcus.com/ca/1/product_assets/B/3/T/5/S/NMB3T5S_au.jpg',
-            title: 'Acne Studios',
-            description: 'Lain pop sky blue',
-            price: 'R$70,00',
-          }
-        ],
-    };
-*/
     render() {
         return (
           <View style={styles.container}>
